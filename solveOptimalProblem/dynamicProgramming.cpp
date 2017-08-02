48785eb1f3175173fe195b7734dcca5c6279e302
yawenChen <elfairyhyuk@gmail.com>
2017-08-02 09:25:38 +0800

    add print optimal solution

--- solveOptimalProblem/dynamicProgramming.cpp
+++ solveOptimalProblem/dynamicProgramming.cpp
@@ -9,30 +9,46 @@ Description: DP is a method to solve optimal problem,the problem should has two 
 using namespace std;
 /*matrix-chain multiplicion,*/
 /*split rod,get most profit*/
-int cutRodMemoUpBottom(int n,vector< int> price,vector<int> &r){
+void printOptimalSolution(vector<int> s, int n){
+	while(n>0){
+		cout<<s[n]<<endl;
+		n=n-s[n];
+	}
+}
+int cutRodMemoUpBottom(int n,vector< int> price,vector<int> &r, vector<int> &s){
 	if(r[n]>=0)
 		return r[n];
 	int max=-1;
 	int q;
 	if(n==0)
 		return 0;
-	else{
-		for(int i=0;i<=n;i++){
-		max=max>(q=price[i]+cutRodMemoUpBottom(n-i,price,r))?max:q;
+	int i;
+	for(i=1;i<=n;i++){
+		q=price[i]+cutRodMemoUpBottom(n-i,price,r,s);
+		if(max<q){
+			max=q;
+			s[n]=i;
 		}
 	}
+	
 	r[n]=max;
+	
 	return r[n];
 }
-/*use up to bottom method, n is lengh of rod, use recurse*/
+/*method 1: use up to bottom method, n is lengh of rod, use recurse*/
 int cutRodMemo(int n,vector<int> price){
-	vector<int> r;
-	for(int i=0;i<n;i++)
+	vector<int> r;//r[i] stroe the optimal profit when n=i
+	vector<int> s;//s[i] stroe the optimal solution when n=i
+	for(int i=0;i<=n;i++){//r.size()=n+1
 		r.push_back(-1);
+		s.push_back(-1);
+	}
 	
-	return cutRodMemoUpBottom(n,price,r);//return r[n]
+	r[n]=cutRodMemoUpBottom(n,price,r,s);//return r[n]
+	printOptimalSolution(s,n);
+	return r[n];
 }
-/*from botoom to up, no recurse. first get max profit when n=1, then when n=2.... */
+/*mwthod 2: from botoom to up, no recurse. first get max profit when n=1, then when n=2.... */
 int cutRodBottomUp(int n,vector<int> price){
 	int max=-1;
 	vector<int> r(n,0);
@@ -57,6 +73,6 @@ int main(){
 	price.push_back(20);
 	price.push_back(24);
 	price.push_back(30);
-	cout<<cutRodBottomUp(6,price)<<endl;
+	cout<<cutRodMemo(6,price)<<endl;
 	return 0;
 }
